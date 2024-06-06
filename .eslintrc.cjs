bf10c887a5e1d78935d27794aaff68f5959cdf93
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-06-06 17:27:27 +0300

    initialized project structure

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,14 +8,26 @@ module.exports = {
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-refresh', 'react-hooks'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    quotes: ['warn', 'single', 'double'],
+    semi: ['warn', 'always'],
+    'no-console': 1,
   },
-}
+};
