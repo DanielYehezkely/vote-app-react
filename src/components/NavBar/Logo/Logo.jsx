5f2f80ba5b00d03aaf3cba42ca5654486e9e689b
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-06-07 18:26:50 +0300

    fixing bugs in logic

--- src/components/NavBar/Logo/Logo.jsx
+++ src/components/NavBar/Logo/Logo.jsx
@@ -4,7 +4,7 @@ import './Logo.css'
 
 const Logo = () => {
   return (
-    <div className="logo">Anime <i className="logo-icon"><img src="/public/assets/images/Katana-PNG-Photos.png" alt="" /></i> <span>V</span>oter</div>
+    <div className="logo">Anime <i className="logo-icon"><img src="/assets/images/Katana-PNG-Photos.png" alt="" /></i> <span>V</span>oter</div>
   );
 };
 
