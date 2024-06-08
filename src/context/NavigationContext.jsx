6e1b9f225d2f79d3d1c546dc86fa56bc9a6bd88c
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-06-08 12:15:00 +0300

    fixing bugs

--- src/context/NavigationContext.jsx
+++ src/context/NavigationContext.jsx
@@ -4,7 +4,7 @@ const NavigationContext = createContext();
 
 export const NavigationProvider = ({ children }) => {
 
-  const [currentPage, setCurrentPage] = useState('admin');
+  const [currentPage, setCurrentPage] = useState('form');
 
 
   return <NavigationContext.Provider value={{currentPage, setCurrentPage}}  >
